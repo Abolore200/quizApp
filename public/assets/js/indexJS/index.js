5da4f72d15e6209787bfb50c360f32ce6516e9a0
Your Name201~ <abolore200@gmail.com>
2023-05-08 08:54:19 +0100

    added share button

--- public/assets/js/indexJS/index.js
+++ public/assets/js/indexJS/index.js
@@ -2,9 +2,19 @@ const options = document.querySelectorAll('.options-home ul li')
 options[2].id = 'exit'
 
 const exitBtn = document.querySelector('#exit')
-//when clicked, exit the page
 exitBtn.addEventListener('click', () => {
+    //when clicked, exit the page
     window.location.href = 'index.html'
 
+    //when clicked, clear the local storage
     localStorage.removeItem('score')
 })
+
+const siteUrl = 'aboquizapp.netlify.app'
+options[1].innerHTML = `
+    <a
+        href="https://twitter.com/intent/tweet?text=Visit%20our%20website%20to%20play%20at%20${siteUrl}"
+        target="_blank">
+            Share via ...
+    </a>
+`
